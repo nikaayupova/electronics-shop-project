aec4b69c61eefbf93e784d126a13a5836d5ee9aa
Nika Ayupova <ayupova.nik@yandex.ru>
2023-07-01 18:44:07 +0300

    HW_1 tests

---
+++ tests/conftest.py
@@ -0,0 +1,6 @@
+import pytest
+
+from src.item import Item
+@pytest.fixture
+def item():
+    return Item("Смартфон", 10000, 20)
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,17 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+from src.item import Item
+class TestItem:
+    def test_item(self, item):
+        assert item.price == 10000
+        assert item.name == "Смартфон"
+        assert item.quantity == 20
+        assert Item.all == [item]
+
+    def test_calculate_total_price(self, item):
+        assert item.calculate_total_price == 200000
+
+    def test_apply_discount(self, item):
+        item.pay_rate = 0.8
+        item.apply_discount()
+        assert item.price() == 8000
