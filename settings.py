627b50958706c9d00cca01103b69c44448ce89e0
Nika Ayupova <ayupova.nik@yandex.ru>
2023-07-08 11:50:45 +0300

    HW_2

--- settings.py
+++ settings.py
@@ -2,4 +2,4 @@ from pathlib import Path
 
 ROOT_PATH = Path(__file__).parent
 
-PATH_CSV_ITEMS = Path.joinpath(ROOT_PATH, 'src', 'items.csv' )
+PATH_CSV_ITEMS = Path.joinpath(ROOT_PATH, 'src', 'items.csv')
